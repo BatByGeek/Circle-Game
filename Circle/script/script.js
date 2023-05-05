720a158ca986c6fbd323378cba34cc9a2424f0e9
Baatyr Beishenaliev <90051654+BatByGeek@users.noreply.github.com>
2023-05-05 16:48:21 +0600

    Add files via upload

---
+++ Circle/script/script.js
@@ -0,0 +1,45 @@
+const circle = document.querySelector('.circle');
+const greenCircle = document.querySelector('.green');
+const redCircle = document.querySelector('.red');
+const blueCircle = document.querySelector('.blue');
+const yellowCircle = document.querySelector('.yellow');
+const pinkCircle = document.querySelector('.pink');
+const orangeCircle = document.querySelector('.orange');
+const purpleCircle = document.querySelector('.purple');
+const brownCircle = document.querySelector('.brown');
+
+greenCircle.addEventListener('click', function() {
+    circle.style.backgroundColor = 'green';
+})
+
+redCircle.addEventListener('click', function() {
+    circle.style.backgroundColor = 'red';
+})
+
+blueCircle.addEventListener('click', function() {
+    circle.style.backgroundColor = 'blue';
+})
+
+blueCircle.addEventListener('click', function() {
+    circle.style.backgroundColor = 'blue';
+})
+
+yellowCircle.addEventListener('click', function() {
+    circle.style.backgroundColor = 'yellow';
+})
+
+pinkCircle.addEventListener('click', function() {
+    circle.style.backgroundColor = 'pink';
+})
+
+orangeCircle.addEventListener('click', function() {
+    circle.style.backgroundColor = 'orange';
+})
+
+purpleCircle.addEventListener('click', function() {
+    circle.style.backgroundColor = 'purple';
+})
+
+brownCircle.addEventListener('click', function() {
+    circle.style.backgroundColor = 'brown';
+})
